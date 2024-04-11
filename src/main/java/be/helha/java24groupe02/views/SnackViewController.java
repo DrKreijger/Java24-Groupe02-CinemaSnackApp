6b87ecb7a4bf5571042e9a497e4c9148e5b47fe3
DrKreijger <sebdek7@gmail.com>
2024-04-11 13:49:25 +0200

    Update SnackViewController.java

--- src/main/java/be/helha/java24groupe02/views/SnackViewController.java
+++ src/main/java/be/helha/java24groupe02/views/SnackViewController.java
@@ -230,7 +230,7 @@ public class SnackViewController {
      */
     private void loadProductsFromDatabase() {
         try {
-            Connection connection = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\DrKreijger\\Documents\\Cours\\Bloc 2 Desktop\\Java24-Groupe02\\snacks_simple.db");
+            Connection connection = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\Dr.Kreijger\\Documents\\Cours\\Bloc 2 V2\\Java24-Groupe02\\snacks_simple.db");
             PreparedStatement statement = connection.prepareStatement(
                     "SELECT * FROM Products"
             );
