5509b8161e6044b908ca3c277e89ee2fe33fd3dd
DrKreijger <sebdek7@gmail.com>
2024-04-17 13:49:47 +0200

    WIP MVC

--- src/main/java/be/helha/java24groupe02/controllers/MainController.java
+++ src/main/java/be/helha/java24groupe02/controllers/MainController.java
@@ -16,6 +16,7 @@ import java.util.List;
 
 public class MainController extends Application implements SnackViewListener {
 
+    SnackViewController snackViewController;
     private Stage currentStage;
     ProductDB productDB;
     private List<Product> products;
@@ -24,18 +25,20 @@ public class MainController extends Application implements SnackViewListener {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        currentStage = primaryStage;
+        this.currentStage = primaryStage;
         productDB = new ProductDB();
         products = productDB.getAllProductsFromDatabase();
         cart = new Cart();
         try {
-            FXMLLoader loader = new FXMLLoader(SnackViewController.class.getResource("SnacksView.fxml"));
-            Parent root = loader.load();
-            SnackViewController controller = loader.getController();
+            FXMLLoader fxmlLoader = new FXMLLoader(SnackViewController.class.getResource("SnacksView.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+
+            SnackViewController controller = fxmlLoader.getController();
             controller.setListener(this);
             controller.setProducts(products);
+
             primaryStage.setTitle("Snacks App");
-            primaryStage.setScene(new Scene(root));
+            primaryStage.setScene(scene);
             primaryStage.show();
         } catch (IOException e) {
             showErrorAlert();
