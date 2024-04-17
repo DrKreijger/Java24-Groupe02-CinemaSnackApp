489bfab15e0c50868e1f9fd7e9766c1c667f47ea
DrKreijger <sebdek7@gmail.com>
2024-04-17 14:00:59 +0200

    Updated product creation

--- src/main/java/be/helha/java24groupe02/models/Product.java
+++ src/main/java/be/helha/java24groupe02/models/Product.java
@@ -4,10 +4,20 @@ public class Product {
     private int id;
     private String name;
     private double price;
-    private String category;
     private String imagePath;
     private String flavor;
     private String size;
+    private int quantity;
+
+    public Product(int id, String name, double price, String imagePath, String flavor, String size) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.imagePath = imagePath;
+        this.flavor = flavor;
+        this.size = size;
+        this.quantity = 1;
+    }
 
     public int getId() {
         return id;
@@ -33,14 +43,6 @@ public class Product {
         this.price = price;
     }
 
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
     public String getImagePath() {
         return imagePath;
     }
@@ -63,5 +65,13 @@ public class Product {
 
     public String getSize() {
         return size;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
     }
 }
